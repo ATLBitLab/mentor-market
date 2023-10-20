b6aebe4036ef55697533965eea3d628d161f0588
Stephen DeLorme <stephen@d.elor.me>
2023-10-20 15:25:51 -0400

    added event creation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
-import { generatePrivateKey, getPublicKey } from 'nostr-tools'
+import { generatePrivateKey, getPublicKey, validateEvent, verifySignature, getSignature, getEventHash, signEvent, getBlankEvent, finishEvent } from 'nostr-tools'
 import { useState, useEffect } from 'react'
+import type { Event, EventTemplate, UnsignedEvent } from 'nostr-tools'
 
 export default function Home() {
   const [sk, setSk] = useState<string | null>('')
   const [pk, setPk] = useState<string | null>('')
-
+  const [eventContent, setEventContent] = useState<string>('')
+  
   useEffect(() => {
     if( [null,''].includes(localStorage.getItem('sk')) ) {
       createKeys();
@@ -13,6 +15,16 @@ export default function Home() {
     else {
       setSk(localStorage.getItem('sk'))
       setPk(localStorage.getItem('pk'))
+
+      if(sk){
+        // Testing event creation
+        let event1 = createEvent('Hello World 1', sk)
+        let event2 = createEvent('Hello World 2', sk)
+        let event3 = createEvent('Hello World 3', sk)
+        console.log(event1)
+        console.log(event2)
+        console.log(event3)
+      }
     }
   }, [])
 
@@ -27,10 +39,43 @@ export default function Home() {
     localStorage.setItem('pk', publicKey)
   }
 
+  
+
+  function createEvent(content: string, sk:string, kind:number = 1, created_at:number = Math.floor(Date.now() / 1000)):Event{
+    let eventTemp = getBlankEvent(kind)
+    eventTemp.content = content
+    eventTemp.created_at = created_at
+    let event = finishEvent(eventTemp, sk || '')
+    console.log(event)
+    return event;
+  }
+
+
   return (
     <main>
-      <p>{sk}</p>
-      <p>{pk}</p>
+      {sk && pk ?
+      <>
+        <h1>Welcome back to Nostr!</h1>
+        <h2>Make an Event</h2>
+
+        <form>
+          <input type="text" placeholder="Event Content" value={eventContent} onChange={(e)=>setEventContent(e.target.value)} />
+          <button type="button" onClick={()=>createEvent(eventContent, sk)}>
+            Make Event
+          </button>
+        </form>
+      </>
+      :
+      <>
+        Generating keys&hellip;
+      </>
+      }
+
+      
+
+      
+
+
     </main>
   )
 }
