d2872b892ea9091d06e2a5bf597a118849edeb44
Stephen DeLorme <stephen@d.elor.me>
2023-10-20 14:06:14 -0400

    cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,35 @@
 import { generatePrivateKey, getPublicKey } from 'nostr-tools'
 import { useState, useEffect } from 'react'
 
-
 export default function Home() {
-  const [sk, setSk] = useState('')
-  const [pk, setPk] = useState('')
-
-  let secretKey = generatePrivateKey()
-  let publicKey = getPublicKey(secretKey)
+  const [sk, setSk] = useState<string | null>('')
+  const [pk, setPk] = useState<string | null>('')
 
   useEffect(() => {
+    if( [null,''].includes(localStorage.getItem('sk')) ) {
+      createKeys();
+    }
+    else {
+      setSk(localStorage.getItem('sk'))
+      setPk(localStorage.getItem('pk'))
+    }
+  }, [])
+
+  function createKeys(){
+    let secretKey = generatePrivateKey()
+    let publicKey = getPublicKey(secretKey)
     setSk(secretKey)
     setPk(publicKey)
     console.log(secretKey)
     console.log(publicKey)
-  }, [])
+    localStorage.setItem('sk', secretKey)
+    localStorage.setItem('pk', publicKey)
+  }
 
   return (
     <main>
-      <p>{pk}</p>
       <p>{sk}</p>
+      <p>{pk}</p>
     </main>
   )
 }
